9579f0fe02c4de09cfa87fa113dc00c3efbf9be5
MtSoftDevSolutions <87912940+MtSoftDevSolutions@users.noreply.github.com>
2021-10-20 11:30:51 -0400

    Learning Data Types

---
+++ VariablesandDataTypes/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace VariablesandDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is a simple program
+            //console.writeline("what is your name ?");
+            //string yourname = console.readline();
+            //console.writeline("your name is: " + yourname);
+            //console.readline();
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+            //bool isStudying = false;
+            //byte hoursWorked = 42;
+            //sbyte currentTemperature = -22;
+            //char questionMark = '\u2103';
+            //decimal moneyInBank = 100.5m;
+            //double heightInCentimeters = 200.00456;
+            //float secondsLeft = 15.56f;
+            //short temperatureOnMars = -301;
+            //string myName = "Michael";
+            //int currentAge = 30;
+            //string yearsOld = currentAge.ToString();
+            //bool isRaining = true;
+            //string rainingStatus = Convert.ToString(isRaining);
+            //Console.WriteLine(questionMark);
+            //Console.ReadLine();
+
+
+        }
+    }
+}
