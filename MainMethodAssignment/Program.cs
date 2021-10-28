f3c6a7090ebfa2ecedb10ebbe7ffc4535e765c49
Michael Tabb <mhtabb.6@gmail.com>
2021-10-28 14:31:07 -0700

    Finished Main Method Assignment

---
+++ MainMethodAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //taking an int value, outputting an int value
+            Console.WriteLine("Input a whole number: ");
+            int add = Convert.ToInt32(Console.ReadLine());
+            int x = mainMethod.addNumber(add);
+            Console.WriteLine(x);
+            
+            //taking a decimal, outputting an int 
+            Console.WriteLine("I demand a decimal. ");
+            decimal div = Convert.ToDecimal(Console.ReadLine());
+            int y = mainMethod.addNumber(div);
+            Console.WriteLine(y);
+            
+            //taking a string putting out an int
+            Console.WriteLine("Your decimal was tasty, can I have another whole number?");
+            string hi = Console.ReadLine();
+            int z = mainMethod.addNumber(hi);
+            Console.WriteLine(z);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MainMethodAssignment/mainMethod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class mainMethod
+    {
+        //this one was from into to int
+        public static int addNumber (int add)
+        {
+            return (add + 7);
+        }
+
+        //this one goes from decimal to int
+        public static int addNumber(decimal div)
+        {
+            int result = Convert.ToInt32(div);
+            return (50 / result);
+
+
+        }
+        //this one took a string and converted it to an int
+        public static int addNumber(string hi)
+        {
+            int stringy = Convert.ToInt32(hi);
+
+            return (stringy * 7);
+        }
+    }
+}
