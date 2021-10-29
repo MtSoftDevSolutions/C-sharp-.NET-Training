b90026da8750a3f313569610a65ecb56505f3268
Michael Tabb <mhtabb.6@gmail.com>
2021-10-29 11:58:37 -0700

    Class work

---
+++ classSub/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classSub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //created a class in divide2 that divides a number input from user by 2.
+            divide2 now = new divide2();
+            Console.WriteLine("Please give me a whole number you\'d like divided by 2: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = 2;
+            now.divideBy(a, b);
+            Console.ReadLine();
+            //This is what is needed in the main program.cs in order to have an example of output parameters
+            Console.WriteLine("Please enter your favorite number: ");
+            bool isValid = int.TryParse(Console.ReadLine(), out int number);
+            string message = isValid ? $"Your favorite number is {number}." : "Please enter a valid number";
+            Console.WriteLine("What is your name? ");
+            string Name = Console.ReadLine();
+            Console.WriteLine($"Your name is {Name}. " + message);
+            Console.ReadLine();
+            //This overloads divide2 because I changed the data type from int to double
+            divide2 when = new divide2();
+            Console.WriteLine("Please give me any number: ");
+            double zed = Double.Parse(Console.ReadLine());
+            double bed = 3.45;
+            when.divideBy(zed, bed);
+            Console.ReadLine();
+            //This is using a static class
+            Console.WriteLine("How many people do you have in your class? ");
+            int classSize = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How many classes do you have? ");
+            int amtofClasses= Convert.ToInt32(Console.ReadLine());
+            //This is still using the same static class plus what I learned from output parameters
+            var result = Classroom.schoolSum(classSize, amtofClasses);
+            Console.WriteLine($"You have {classSize} students in your class and {amtofClasses} classes.\n " +
+                $"That totals to {result} students.\n Geesh it\'s time for a vacation!");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ classSub/divide2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classSub
+{
+    class divide2
+    {
+        //The first method is just dividing by 2. 
+        public void divideBy(int a, int b)
+        {
+            b = 2;
+            Console.WriteLine("Your number divided by 2 is: " + (a / b));
+        }
+        //this method is an example of output parameters
+        public void bio(out int number, out string Name)
+        {
+            number = Convert.ToInt32(Console.ReadLine());
+            Name = Console.ReadLine();
+            Console.WriteLine($"Your name is {Name}. Your favorite number is {number}");
+        }
+        //this is overloading divideBy because I changed the data type
+        public void divideBy(double a, double b)
+        {
+            b = 3.45;
+            Console.WriteLine("Your number divided by my number is: " + (a / b));
+        }
+  
+    }
+    //This is making a static class. Static classes can't be instantiated which seems to make things easier.
+    public static class Classroom
+    {
+        public static int resultStorage = 0;
+        public static int classSize;
+        public static int amtofClasses;
+        public static string classType = "Math";
+        public static int schoolSum(int classSize, int amtofClasses)
+        {
+            return classSize * amtofClasses;
+        }
+        public static void Huge(int result)
+        {
+            resultStorage = result;
+        }
+    }
+}
