51f191cbf77479709fdc0690de22e84cca240ade
MtSoftDevSolutions <87912940+MtSoftDevSolutions@users.noreply.github.com>
2021-11-03 16:50:02 -0400

    Add files via upload

---
+++ parsingEnums/parsingEnums/MikeClassEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace parsingEnums
+{
+    public class MikeClassEnum
+    {
+       //giving property to class
+       public TimeOfTheDay time { get; set; } 
+    }
+    //Created enums for times of the day
+    public enum TimeOfTheDay
+    {
+        morning,
+        afternoon,
+        evening,
+        night
+
+    }
+}
---
+++ parsingEnums/parsingEnums/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace parsingEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try catch for user errors
+            try
+            {
+                //instantiated the MikeClassEnum
+                MikeClassEnum variable = new MikeClassEnum();
+                //Ask user the time of day
+                Console.WriteLine("Is it morning, afternoon, evening, or night time?");
+                string userInput = Console.ReadLine();
+                variable.time = (TimeOfTheDay)Enum.Parse(typeof(TimeOfTheDay), userInput);
+                Console.WriteLine($"Yes, it's {userInput} time.");
+            }
+            catch (System.ArgumentException ex)
+            {
+                Console.WriteLine("Please enter a time of day. (example, morning)");
+            }
+            Console.ReadLine();
+        }
+    }
+}
