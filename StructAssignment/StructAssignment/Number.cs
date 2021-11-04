f2c7c7dca775add53f2274fb5a24b68d06aa4575
codepadiwan <kaig286@outlook.com>
2021-11-04 13:18:42 -0400

    test

---
+++ StructAssignment/StructAssignment/Number.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace StructAssignment
+{
+    //Created a struct called Number
+    public struct Number
+    {
+        //Gave it the property Amount with the data type decimal
+        public decimal Amount;
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the struct Number into Main()
+            Number number = new Number();
+
+            //Assigned an amount to number
+            number.Amount = 4.56m;
+
+            Console.WriteLine("The amount is " + number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
